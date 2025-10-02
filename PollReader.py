599487a7d0d3a45b0fa7a64627dfc8c493f1ebd0
Rohan Karunakaran <rohanvk@umich.edu>
2025-10-02 09:35:51 -0400

    Fixed bugs and implemented highest polling candidate method

--- PollReader.py
+++ PollReader.py
@@ -55,16 +55,15 @@ class PollReader():
         """
 
         # iterate through each row of the data
-        for i in self.raw_data:
-
-            # split up the row by column
-            seperated = i.split(' ')
+        for row in self.raw_data[1:]:
+            # split the row by comma
+            seperated = row.strip().split(',')
 
             # map each part of the row to the correct column
             self.data_dict['month'].append(seperated[0])
             self.data_dict['date'].append(int(seperated[1]))
-            self.data_dict['sample'].append(int(seperated[2]))
-            self.data_dict['sample type'].append(seperated[2])
+            self.data_dict['sample'].append(int(seperated[2].split(" ")[0]))
+            self.data_dict['sample type'].append(seperated[2].split(" ")[1])
             self.data_dict['Harris result'].append(float(seperated[3]))
             self.data_dict['Trump result'].append(float(seperated[4]))
 
@@ -80,7 +79,16 @@ class PollReader():
             str: A string indicating the candidate with the highest polling percentage or EVEN,
              and the highest polling percentage.
         """
-        pass
+        max_harris = max(self.data_dict['Harris result'])
+        max_trump = max(self.data_dict['Trump result'])
+
+        if max_harris > max_trump:
+            return f"Harris { (max_harris * 100):.1f}%"
+        elif max_trump > max_harris:
+            return f"Trump { (max_trump * 100):.1f}%"
+        else:
+            return f"EVEN { (max_harris * 100):.1f}"
+
 
 
     def likely_voter_polling_average(self):
@@ -91,7 +99,7 @@ class PollReader():
             tuple: A tuple containing the average polling percentages for Harris and Trump
                    among likely voters, in that order.
         """
-        pass
+        
 
 
     def polling_history_change(self):
